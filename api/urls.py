a8ef63eb7c256c7f61974671d47afef9c1f3b506
Sidhartha Singh <sidhartha@Sidharthas-MacBook-Air.local>
2026-05-10 21:32:25 +0530

    ft-updated root url

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path("health/", views.health),
     path("users/", views.users),
+    path("media/presigned-url/", views.make_upload_url),
     path("posts/", views.posts),
+    path("posts/search/", views.search_posts),
+    path("posts/trending/top10/", views.top_trending),
     path("posts/trending/", views.trending),
     path("posts/<int:post_id>/", views.one_post),
 ]
